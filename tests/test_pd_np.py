f8c78af11a0a8d6023d719a21357949a7c9a237a
Kota Mori <kota.mori@thinkbiganalytics.com>
2017-10-26 12:37:15 +0900

    #10 skip pandas test for python <= 2.6

--- tests/test_pd_np.py
+++ tests/test_pd_np.py
@@ -3,13 +3,23 @@
 
 import unittest
 import re
-import numpy as np
-import pandas as pd
+import sys
 from mecabwrap.domecab import do_mecab_vec
+import numpy as np
+
+# pandas import may fail with python 2.6 due to
+# 'cannot import name OrderedDict' or similar
+# skip pandas test for python 2.6 or lower
+PY26 = sys.version_info[0] == 2 and sys.version_info[0] <= 6
+if not PY26:
+    import pandas as pd
 
 
 class TestPdNp(unittest.TestCase):
     def test_pandas(self):
+        if PY26:
+            return
+            
         x1 = [u'会いたくて', u'会いたくて', u'震える']
         x2 = pd.Series(x1)
         
