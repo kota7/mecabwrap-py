e1adec1366264fb23789b0825254fc43b0130510
Kota Mori <kota.mori@thinkbiganalytics.com>
2017-10-26 11:12:56 +0900

    #10 syntax error fix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
 
     packages=['mecabwrap'],
     install_requires=[],
-    test_require=['pandas', 'numpy']
+    test_require=['pandas', 'numpy'],
     package_data={},
     entry_points={},
     
