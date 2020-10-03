deb51e155196f3adfb652d4d61eb3340ca2f040c
Kota Mori <kmori05@gmail.com>
2020-10-04 02:24:57 +0900

    version 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mecabwrap',
-    version='0.2.3',
+    version='0.3.0',
     description='Yet another interface to MeCab morphological analyzer',
     author='Kota Mori', 
     author_email='kmori05@gmail.com',
