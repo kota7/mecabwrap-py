3a4c83b35cf5314613c9317892742dc9188529eb
Kota Mori <kota7@users.noreply.github.com>
2020-11-29 12:31:17 +0900

    fix v0.3.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(readmefile) as f:
 
 setup(
     name='mecabwrap',
-    version='0.3.2dev',
+    version='0.3.2',
     description='Yet another interface to MeCab morphological analyzer',
     author='Kota Mori', 
     author_email='kmori05@gmail.com',
